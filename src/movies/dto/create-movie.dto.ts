818632a8a14858473889e172b5df7c43d64fd59a
Marina <work.m777553@gmail.com>
2026-02-15 16:24:53 +0300

    STEP-4 add validation

--- src/movies/dto/create-movie.dto.ts
+++ src/movies/dto/create-movie.dto.ts
@@ -1,6 +1,30 @@
+import {
+  IsInt,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+  Min,
+  MinLength,
+} from 'class-validator';
+
 export class CreateMovieDto {
+  @IsString()
+  @IsNotEmpty({ message: 'Title is required field' })
+  @MinLength(1)
   title: string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'Director is required field' })
   director: string;
+
+  @IsInt()
+  @Min(1895, { message: 'The movie industry starts in 1895' })
   year: number;
+
+  @IsString()
+  @IsOptional()
+  @MinLength(5, { message: 'Need more symbols (5)' })
+  @MaxLength(1000, { message: 'Too long (1000 max)' })
   description: string;
 }
--- src/movies/dto/update-movie.dto.ts
+++ src/movies/dto/update-movie.dto.ts
@@ -1,8 +1,30 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateMovieDto } from './create-movie.dto';
+import {
+  IsBoolean,
+  IsInt,
+  IsOptional,
+  IsString,
+  Max,
+  MaxLength,
+  Min,
+  MinLength,
+} from 'class-validator';
 
 export class UpdateMovieDto extends PartialType(CreateMovieDto) {
+  @IsInt({ message: 'целое число, от 1 до 10' })
+  @Min(1, { message: 'целое число, от 1' })
+  @Max(10, { message: 'целое число, до 10' })
+  @IsOptional()
   rating?: number;
+
+  @IsBoolean()
+  @IsOptional()
   isWatched?: boolean;
+
+  @IsString()
+  @MinLength(5, { message: 'Need more symbols (5)' })
+  @MaxLength(1000, { message: 'Too long (500 max)' })
+  @IsOptional()
   notes?: string;
 }
